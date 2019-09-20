0f5a2eb1c656daaadb27698454b378a7c5607197
gilangsuryalaga <gilangsuryalaga@gmail.com>
2019-09-20 10:12:50 +0700

    all layout

--- app/src/main/java/com/example/dateline/HomeFragment.java
+++ app/src/main/java/com/example/dateline/HomeFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -22,5 +23,4 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home,null);
     }
-
 }
